76cca0ff851288ac8a284e75e08858720dcfba4c
ViniciusTei <viniciustprates@gmail.com>
2021-08-20 09:13:38 -0300

    fix(rss) redirect to show feed page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,18 +3,31 @@ require 'open-uri'
 
 class HomeController < ApplicationController
   def index
-    url = 'https://anchor.fm/s/fa5d110/podcast/rss'
+  end
 
-    URI.open(url) do |rss|
-      feed = RSS::Parser.parse(rss)
-      @podcast = feed.channel
-      @items = feed.items
-      
-    end
+  def show
+    search 
   end
 
   private
-    def homeParams 
-      params.require(:home).permit(:url)
+  def search
+    url = 'https://anchor.fm/s/fa5d110/podcast/rss'
+    begin
+      URI.open(url) do |rss|
+        feed = RSS::Parser.parse(rss)
+        @podcast = feed.channel
+        @items = feed.items
+        render :show
+      end
+    rescue OpenURI::HTTPError => error 
+      raise error
+      render :index
+    end
+    
+  end
+
+  private
+    def rssParams 
+      params.require(:home).permit(:rss_query)
     end
 end
